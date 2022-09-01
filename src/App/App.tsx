c9dd42deeaaad8a8030ec26d76904b0b1f9c5609
TauDuque <hectauduque@gmail.com>
2022-09-01 12:38:58 +0200

    feat: itens da lista dinamicos, lógica com reducers

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import {
   Button,
@@ -8,48 +8,89 @@ import {
   ListGroup,
   ListGroupItem,
 } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { deleteTodo, markCompleto, markIncompleto } from 'action';
+import storeTypes from 'Types/storeTypes';
+import AppPropsTypes from './AppPropsTypes';
 
-const App: React.FC = () => {
+const App: React.FC<AppPropsTypes> = ({
+  completo,
+  incompleto,
+  deleteTodo,
+  markCompleto,
+  markIncompleto,
+}) => {
+  const input = useRef<HTMLInputElement>(null);
+
+  const renderizarLista = (type: 'Completo' | 'Incompleto') => {
+    const looper = type === 'Completo' ? completo : incompleto;
+    return (
+      <ListGroup variant="flush" className="m-2">
+        <h3>{type}</h3>
+        {looper.map((item, i) => {
+          return (
+            <ListGroupItem
+              key={i}
+              variant={type === 'Completo' ? 'success' : 'danger'}
+              style={{ display: 'flex', justifyContent: 'space-between' }}
+            >
+              <div>{item}</div>
+              <div>
+                <i
+                  className={`fas fa-${
+                    type === 'Completo' ? 'minus' : 'check'
+                  } m-2`}
+                  onClick={() => {
+                    type === 'Completo'
+                      ? markIncompleto(item)
+                      : markCompleto(item);
+                  }}
+                ></i>
+                <i
+                  className="fas fa-trash m-2"
+                  onClick={() => deleteTodo(item)}
+                ></i>
+              </div>
+            </ListGroupItem>
+          );
+        })}
+      </ListGroup>
+    );
+  };
+
+  const adicionar = () => {
+    if (input.current) {
+      const valor = input.current.value;
+      markIncompleto(valor);
+    }
+  };
+
   return (
     <Container>
       <InputGroup className="m-3">
-        <FormControl placeholder="Todo" />
+        <FormControl placeholder="Todo" ref={input} />
         <InputGroup.Append>
-          <Button variant="secondary">
+          <Button variant="secondary" onClick={() => adicionar()}>
             <i className="fas fa-plus mr-1"></i>
             Adicionar
           </Button>
         </InputGroup.Append>
       </InputGroup>
-      <ListGroup variant="flush" className="m-2">
-        <h3>Incompleto</h3>
-        <ListGroupItem
-          variant="danger"
-          style={{ display: 'flex', justifyContent: 'space-between' }}
-        >
-          <div>Cras justo odio</div>
-          <div>
-            <i className="fas fa-check m-2"></i>
-            <i className="fas fa-trash m-2"></i>
-          </div>
-        </ListGroupItem>
-      </ListGroup>
-      <ListGroup variant="flush" className="m-2">
-        <h3>Completo</h3>
-        <ListGroupItem
-          variant="success"
-          style={{ display: 'flex', justifyContent: 'space-between' }}
-          action
-        >
-          <div>Cras justo odio</div>
-          <div>
-            <i className="fas fa-minus m-2"></i>
-            <i className="fas fa-trash m-2"></i>
-          </div>
-        </ListGroupItem>
-      </ListGroup>
+      {renderizarLista('Completo')}
+      {renderizarLista('Incompleto')}
     </Container>
   );
 };
 
-export default App;
+const mapStateToProps = (state: storeTypes) => {
+  return {
+    completo: state.completo,
+    incompleto: state.incompleto,
+  };
+};
+
+export default connect(mapStateToProps, {
+  deleteTodo,
+  markCompleto,
+  markIncompleto,
+})(App);
