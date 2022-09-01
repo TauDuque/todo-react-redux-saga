01d495e45b9cba3170b74b972c4cffc06aa78054
TauDuque <hectauduque@gmail.com>
2022-09-01 11:34:55 +0200

    feat: tipagem de store

---
+++ src/Types/storeTypes.ts
@@ -0,0 +1,10 @@
+// tipagem de stores
+export type completo = string[];
+export type incompleto = string[];
+
+interface storeTypes {
+  completo: completo;
+  incompleto: incompleto;
+}
+
+export default storeTypes;
